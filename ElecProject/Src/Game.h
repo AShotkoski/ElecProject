ae3b0577513d0b2c61d3e78cf3f84d82dac0fba6
Alop Slop <Shotkoskialex@gmail.com>
2024-10-22 17:23:15 -0600

    Added some help and adjusted the window title

--- ElecProject/Src/Game.h
+++ ElecProject/Src/Game.h
@@ -32,7 +32,7 @@ private:
 private:
 	static constexpr UINT ScreenWidth = 1272u;
 	static constexpr UINT ScreenHeight = 954u;
-	static constexpr const wchar_t* WindowTitle = L"Rendering engine";
+	static constexpr const wchar_t* WindowTitle = L"Rendering engine for Elec 1520";
 	static constexpr float NearClipping = 0.1f;
 	static constexpr float FarClipping = 1300.0f;
 	static constexpr float Fov = 105.f; // degrees
